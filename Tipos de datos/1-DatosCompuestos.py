fb3306c7a636c71f7f9ce020104987d06e8beea9
gabriel <gabrielreve96@gmail.com>
2024-04-22 21:31:22 +0200

    Datos compuestos

---
+++ Tipos de datos/1-DatosCompuestos.py
@@ -0,0 +1,30 @@
+"""
+-Listas:[]
+-Tuplas:() no se puede modficar
+
+"""
+
+lista=["python" , "javascript" ,"c#"]
+
+"obetner datos"
+
+print(lista[0])
+
+"Modificamos un valor"
+
+lista[0]="Ruby"
+
+print(lista[0])
+
+
+
+"recorre tu lista con for "
+for lenguajes in lista:
+    print(f"mis lenguajes favortios son {lenguajes} ")
+
+
+
+
+tupla=("java" , "Go" , "Angular")
+
+print(tupla)
